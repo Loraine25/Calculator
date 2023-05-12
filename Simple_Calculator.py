24da8e22834d0d21bea1a412d34db6b79a22d0ac
unknown <anonuevoloraine6@gmail.com>
2023-05-12 16:07:47 +0800

    perform calculations

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -28,3 +28,39 @@ def calculator():
     # Ask to enter first and second number
     firstnumber = float(input("\033[;33m" "Enter the 1st number : "))
     secondnumber = float(input("\033[;33m" "Enter the 2nd number : "))
+
+    # If the user wants addition
+    if operation == "+":
+        print(firstnumber, "+", secondnumber)
+        sum = firstnumber + secondnumber
+        print("The result is:", "\033[;3m" + str(sum) + "\033[;m")
+
+    # If the user wants subtraction
+    elif operation == "-":
+        print(firstnumber, "-", secondnumber)
+        difference = firstnumber - secondnumber
+        print("The difference:", "\033[;3m" + str(difference) + "\033[;m")
+
+    # If the user wants multiplication
+    elif operation == "*":
+        print(firstnumber, "*", secondnumber)
+        product = firstnumber * secondnumber
+        print("The product:", "\033[;3m" + str(product) + "\033[;m")
+
+    # If the user wants division
+    elif operation == "/":
+        # Exception for division, if the user entered zero(0) as a divisor(2nd number)
+        try:
+            print(firstnumber, "/", secondnumber)
+            quotient = firstnumber / secondnumber
+            print("The quotient:", "\033[;3m" + str(quotient) + "\033[;m")
+
+        except ZeroDivisionError as ERROR:
+            print("\033[1;31m" "Invalid equation\n")
+            print(ERROR)
+            print("\nTry again, please insert a non-zero number")
+
+
+
+calculator()
+
